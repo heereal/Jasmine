aae909d0e921239f7d198086683f274031057346
heeryoung <winterbooks@naver.com>
2023-01-21 11:47:01 +0900

    feat: 지도에 독립서점 위치 마커로 모두 표시하기 성공

--- src/components/Layout/KakaoMap/KakaoMap.tsx
+++ src/components/Layout/KakaoMap/KakaoMap.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import data from "../../../bookstore.json"
 
 declare global {
 	interface Window {
@@ -9,13 +10,36 @@ declare global {
 export default function KaokaoMap () {
 
     const container = useRef(null);
-    let options = {
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3
+
+    const handleMap = () => {
+
+        let options = {
+            center: new window.kakao.maps.LatLng(37.56839464, 126.9303023),
+            level: 7
+        };
+
+        const map =new window.kakao.maps.Map(container.current, options);
+    
+        
+        // var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+        // // 마커 이미지의 이미지 크기 입니다
+        // var imageSize = new kakao.maps.Size(24, 35); 
+        
+        // // 마커 이미지를 생성합니다    
+        // var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+    
+        data.forEach((store) => {
+            new window.kakao.maps.Marker({
+                map: map,
+                title: store.FCLTY_NM,
+                position: new window.kakao.maps.LatLng(store.FCLTY_LA, store.FCLTY_LO)
+            })
+        })
+
     };
 
     useEffect(()=> {
-        new window.kakao.maps.Map(container.current, options);
+        handleMap();
     })
 
     return(
