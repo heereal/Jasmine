3a278ea1e8aefe60c0e47947993ad6d55d8e0aed
heeryoung <winterbooks@naver.com>
2023-01-22 00:15:40 +0900

    feat: 마커 이미지 변경, 줌 컨트롤 생성

--- src/components/Layout/KakaoMap/KakaoMap.tsx
+++ src/components/Layout/KakaoMap/KakaoMap.tsx
@@ -24,24 +24,38 @@ export default function KaokaoMap () {
 
         // 지도를 표시할 div와 지도 옵션으로 지도를 생성함
         const map = new window.kakao.maps.Map(container.current, options); 
+
+        // 지도 확대 축소를 제어할 수 있는 줌 컨트롤 생성
+        var zoomControl = new window.kakao.maps.ZoomControl();
+        map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
     
-        
-        // var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
-        // // 마커 이미지의 이미지 크기 입니다
-        // var imageSize = new kakao.maps.Size(24, 35); 
-        // // 마커 이미지를 생성합니다    
-        // var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+        // 마커 이미지 생성   
+        var imageSrc = "https://user-images.githubusercontent.com/117061017/213868546-c26efb7b-3288-436c-a937-a10780b34cfb.png"; 
+        var imageSize = new window.kakao.maps.Size(30, 30); 
+        var markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize); 
     
         // 마커 여러 개 표시하기
         data.forEach((store) => {
             new window.kakao.maps.Marker({
                 map: map, // 마커를 표시할 지도
                 title: store.FCLTY_NM, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됨
-                position: new window.kakao.maps.LatLng(store.FCLTY_LA, store.FCLTY_LO) // 마커를 표시할 위치(위도, 경도)
+                position: new window.kakao.maps.LatLng(store.FCLTY_LA, store.FCLTY_LO), // 마커를 표시할 위치(위도, 경도)
+                clickable: true,
+                image: markerImage // 커스텀 마커 이미지 설정
             })
         })
 
     };
+
+    // 지도 이동하기
+    function panTo() {
+        // 이동할 위도 경도 위치를 생성합니다 
+        var moveLatLon = new window.kakao.maps.LatLng(33.450580, 126.574942);
+        
+        // 지도 중심을 부드럽게 이동시킵니다
+        // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
+        // map.panTo(moveLatLon);            
+    }        
 
     useEffect(()=> {
         handleMap();
