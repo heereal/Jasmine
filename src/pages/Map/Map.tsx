9a360386d50f57fa8cd7e4aed5e649c9fc569973
MINSUNG KIM <78587041+GhostPines@users.noreply.github.com>
2023-01-25 14:51:09 +0900

    doc: map.tsx에서 발생한 에러코드 덮어쓰기를 통한 해결

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -4,21 +4,13 @@ import styled from 'styled-components';
 import { PAGE_HEIGHT } from '../../common/layout';
 
 export default function Map() {
-  return <KaokaoMap />;
+  return (
+    <S.Container>
+      <InfoWrapper />
+      <MapWrapper />
+    </S.Container>
+  );
 }
-
-const S = {
-  Container: styled.div`
-    display: flex;
-    flex-direction: row;
-    height: ${PAGE_HEIGHT}px;
-    @media screen and (max-width: 768px) {
-      flex-direction: column;
-      height: inherit;
-      overflow-y: auto;
-    }
-  `,
-};
 
 const S = {
   Container: styled.div`
