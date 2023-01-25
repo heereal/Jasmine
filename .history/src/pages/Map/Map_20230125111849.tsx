d0c7dad82f3e18945d80f85faa3858ff4a78d7fd
MINSUNG KIM <78587041+GhostPines@users.noreply.github.com>
2023-01-25 13:41:19 +0900

    feat: HomeUI 기본틀 작성

--- .history/src/pages/Map/Map_20230125111849.tsx
+++
@@ -1,7 +0,0 @@
-import KaokaoMap from "../../components/Layout/KakaoMap/KakaoMap"
-
-export default function Map() {
-
-    return (
-        <KaokaoMap />
-    )
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,20 +8,43 @@ export default function Home() {
       </MainImageLocation>
 
       <MoveToFindBookstore>
-        <BTNMoveToFindBookstore> 🔍독립서점 찾기 </BTNMoveToFindBookstore>
+        <BTNMoveToFindBookstore> 🔍 독립서점 찾기 </BTNMoveToFindBookstore>
       </MoveToFindBookstore>
 
       <SectorIntroduceNewBookStore>
-        <H1IntroduceNewBookStore></H1IntroduceNewBookStore>
+        <H1IntroduceNewBookStore>
+          NEW : 따끈따끈 신규 서점
+        </H1IntroduceNewBookStore>
+        <WrapperCardsIntroduceNewBookStore>
+          <CardIntroductNewBookStore>
+            <H2IntroduceNewBookStore>헬로인디</H2IntroduceNewBookStore>
+            <IMGIntroduceNewBookStore src="images/mainimage.png" alt="" />
+          </CardIntroductNewBookStore>
+
+          <CardIntroductNewBookStore>
+            <H2IntroduceNewBookStore>헬로인디</H2IntroduceNewBookStore>
+            <IMGIntroduceNewBookStore src="images/mainimage.png" alt="" />
+          </CardIntroductNewBookStore>
+
+          <CardIntroductNewBookStore>
+            <H2IntroduceNewBookStore>헬로인디</H2IntroduceNewBookStore>
+            <IMGIntroduceNewBookStore src="images/mainimage.png" alt="" />
+          </CardIntroductNewBookStore>
+
+          <CardIntroductNewBookStore>
+            <H2IntroduceNewBookStore>헬로인디</H2IntroduceNewBookStore>
+            <IMGIntroduceNewBookStore src="images/mainimage.png" alt="" />
+          </CardIntroductNewBookStore>
+        </WrapperCardsIntroduceNewBookStore>
       </SectorIntroduceNewBookStore>
 
       <SectorSubscribeOurLetter>
-        <H1SubscribeOurLetter></H1SubscribeOurLetter>
+        <H1SubscribeOurLetter>소식지를 구독하세요.</H1SubscribeOurLetter>
 
         <SubscribeInputBox>
-          <InputSubscriberName></InputSubscriberName>
-          <InputSubscriberEmail></InputSubscriberEmail>
-          <BTNSubscribeSubmit></BTNSubscribeSubmit>
+          <InputSubscriberName placeholder="이름" />
+          <InputSubscriberEmail placeholder="이메일" />
+          <BTNSubscribeSubmit> 구독 </BTNSubscribeSubmit>
         </SubscribeInputBox>
       </SectorSubscribeOurLetter>
     </HomeContainer>
@@ -41,9 +64,15 @@ const BackGroundImage = styled.img`
   width: 100%;
 `;
 
-const MoveToFindBookstore = styled.div``;
+const MoveToFindBookstore = styled.div`
+  width: 100%;
+  height: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 const BTNMoveToFindBookstore = styled.button`
-  width: 300px;
+  width: 400px;
   height: 80px;
   background: linear-gradient(
     86.82deg,
@@ -53,14 +82,94 @@ const BTNMoveToFindBookstore = styled.button`
   );
   border-radius: 80px;
   border: none;
-  font-size: large;
+  font-size: x-large;
   color: white;
 `;
-const SectorIntroduceNewBookStore = styled.div``;
-const H1IntroduceNewBookStore = styled.div``;
-const SectorSubscribeOurLetter = styled.div``;
-const H1SubscribeOurLetter = styled.div``;
-const SubscribeInputBox = styled.div``;
-const InputSubscriberName = styled.div``;
-const InputSubscriberEmail = styled.div``;
-const BTNSubscribeSubmit = styled.div``;
+const SectorIntroduceNewBookStore = styled.div`
+  width: 100%;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+`;
+const H1IntroduceNewBookStore = styled.div`
+  left: 0;
+  margin-top: 20px;
+  margin-left: 30px;
+  margin-bottom: 30px;
+  position: absolute;
+  font-size: xx-large;
+  font-weight: 900;
+`;
+
+const WrapperCardsIntroduceNewBookStore = styled.div`
+  width: 100%;
+  height: 300px;
+  margin-top: 50px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+`;
+const CardIntroductNewBookStore = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  height: 220px;
+  padding: 20px;
+  margin: 15px;
+`;
+const H2IntroduceNewBookStore = styled.div`
+  font-size: xx-large;
+  font-weight: 900;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+`;
+const IMGIntroduceNewBookStore = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+const SectorSubscribeOurLetter = styled.div`
+  width: 100%;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const H1SubscribeOurLetter = styled.div`
+  position: absolute;
+  left: 0;
+  margin-left: 30px;
+  font-size: x-large;
+`;
+
+const SubscribeInputBox = styled.div`
+  width: 100%;
+  height: 200px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+const InputSubscriberName = styled.input`
+  margin-right: 10px;
+  width: 250px;
+  height: 40px;
+`;
+const InputSubscriberEmail = styled.input`
+  margin-right: 10px;
+  width: 250px;
+  height: 40px;
+  ::placeholder {
+  }
+`;
+const BTNSubscribeSubmit = styled.button`
+  width: 60px;
+  height: 40px;
+  ::placeholder {
+  }
+`;
