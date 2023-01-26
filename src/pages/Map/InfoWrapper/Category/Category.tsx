aa7a17c2df11847ae7f984ae7dbc3cda164a9777
wallofwolf <77008335+wallofwolf@users.noreply.github.com>
2023-01-26 11:02:12 +0900

    feat: 도서 카테고리에 따른 서점 필터링 기능 추가

--- src/pages/Map/InfoWrapper/Category/Category.tsx
+++ src/pages/Map/InfoWrapper/Category/Category.tsx
@@ -2,6 +2,10 @@ import { Dispatch, SetStateAction } from 'react';
 import { BLACK_COLOR, LIGHT_GRAY_COLOR } from '../../../../common/colors';
 import { categories } from '../../../../common/data';
 import * as S from './Category.style';
+import { useRecoilState } from 'recoil';
+import { dbState } from '../../../../store/selectors';
+import { data } from '../../../../bookstore';
+import { IdbState } from '../../../../store/selectors';
 
 interface CategoryProps {
   setOpenCategory: Dispatch<SetStateAction<boolean>>;
@@ -14,6 +18,9 @@ export default function Category({
   currentCategory,
   setCurrentCategory,
 }: CategoryProps) {
+  // 전역 DB
+  const [DB, setDB] = useRecoilState<IdbState[]>(dbState);
+
   // 카테고리 클릭 핸들링 함수
   const handleCategoryClick = (category: string) => {
     // 현재 카테고리와 같은 카테고리를 클릭했을 경우 초기화
@@ -25,6 +32,11 @@ export default function Category({
     // 현재 카테고리와 다른 카테고리를 클릭했을 경우 카테고리 변경
     setCurrentCategory(category);
     setOpenCategory(false);
+
+    //
+    let result = data.filter((item) => item.MLSFC_NM.includes(category));
+
+    setDB(result);
   };
 
   return (
