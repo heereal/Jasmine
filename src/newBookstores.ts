67a6e8ba1eb3b94e03b5bdca85e227374e70ee2e
Hyojin <0910.shj@gmail.com>
2023-01-27 10:45:34 +0900

    feat: 따끈따끈 신규서점 클릭 시 라우팅 기능 추가
    .

---
+++ src/newBookstores.ts
@@ -0,0 +1,24 @@
+const newBookstores = [
+  {
+    name: '한낮의 바다',
+    imgSrc: require('./assets/images/mainimage.png'),
+    id: 'KCIBSPO21N000000001',
+  },
+  {
+    name: '헬로인디',
+    imgSrc: require('./assets/images/mainimage.png'),
+    id: 'KCIBSPO21N000000001',
+  },
+  {
+    name: '헬로인디',
+    imgSrc: require('./assets/images/mainimage.png'),
+    id: 'KCIBSPO21N000000001',
+  },
+  {
+    name: '토마토',
+    imgSrc: require('./assets/images/mainimage.png'),
+    id: 'KCIBSPO21N000000001',
+  },
+];
+
+export default newBookstores;
