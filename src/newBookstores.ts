091b3eefff53db8ace9a6fedb7f0220701255a89
Hyojin <0910.shj@gmail.com>
2023-01-27 11:35:18 +0900

    design: HomeUI 디자인 수정
    .

--- src/newBookstores.ts
+++ src/newBookstores.ts
@@ -1,22 +1,22 @@
 const newBookstores = [
   {
     name: '한낮의 바다',
-    imgSrc: require('./assets/images/mainimage.png'),
+    imgSrc: require('./assets/images/mainimage1.jpeg'),
     id: 'KCIBSPO21N000000001',
   },
   {
     name: '헬로인디',
-    imgSrc: require('./assets/images/mainimage.png'),
+    imgSrc: require('./assets/images/mainimage1.jpeg'),
     id: 'KCIBSPO21N000000001',
   },
   {
     name: '헬로인디',
-    imgSrc: require('./assets/images/mainimage.png'),
+    imgSrc: require('./assets/images/mainimage1.jpeg'),
     id: 'KCIBSPO21N000000001',
   },
   {
     name: '토마토',
-    imgSrc: require('./assets/images/mainimage.png'),
+    imgSrc: require('./assets/images/mainimage1.jpeg'),
     id: 'KCIBSPO21N000000001',
   },
 ];
--- src/pages/Home/Home.styles.tsx
+++ src/pages/Home/Home.styles.tsx
@@ -1,16 +1,23 @@
 import styled from 'styled-components';
 
 export const HomeContainer = styled.div`
+  font-family: 'Pretendard-Regular';
   height: 100%;
   width: 100%;
 `;
 export const MainImageLocation = styled.div`
+  position: relative;
   height: 100%;
   width: 100%;
 `;
 export const BackGroundImage = styled.img`
-  height: 100%;
-
   width: 100%;
-  max-width: 1200px;
+  height: 60vh;
+  object-fit: cover;
+`;
+export const Jasmin = styled.img`
+  position: absolute;
+  height: 70%;
+  right: 0;
+  bottom: 3px;
 `;
